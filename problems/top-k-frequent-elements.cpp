339b08c93277bd8e411f806c292ec053290d889f
zo0005 <aaaaaright123@gmail.com>
2020-02-15 16:04:55 +0900

    add Top K Frequent Elements

---
+++ problems/top-k-frequent-elements.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    vector<int> topKFrequent(vector<int> &nums, int k) {
+        unordered_map<int, int> map;
+        for(int num : nums) {
+            map[num]++;
+        }
+
+        vector<int> res;
+
+        priority_queue<pair<int, int>> pq;
+        for(auto it = map.begin(); it != map.end(); it++) {
+            pq.push(make_pair(it->second, it->first));
+
+            if(res.size() == k) {
+                res.push_back(pq.top().second);
+                pq.pop();
+            }
+        }
+        return res;
+    }
+};
