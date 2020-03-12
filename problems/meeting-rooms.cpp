e92793550619bdaac166568eba23dbcbd43ea9ed
zo0005 <aaaaaright123@gmail.com>
2020-03-12 19:54:20 +0900

    add Meeting Rooms

---
+++ problems/meeting-rooms.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    bool canAttendMeetings(vector<vector<int>> &intervals) {
+        if(intervals.size() == 0) {
+            return true;
+        }
+
+        sort(intervals.begin(), intervals.end());
+
+        for(int i = 0; i < intervals.size() - 1; i++) {
+            if(intervals[i][1] > intervals[i + 1][0]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
