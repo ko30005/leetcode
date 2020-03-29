84f14fc19e4174ea7bedf5e0536c4c151debb979
zo0005 <aaaaaright123@gmail.com>
2020-03-29 18:23:33 +0900

    add Kth Largest Element in an Array

---
+++ problems/kth-largest-element-in-an-array.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int findKthLargest(vector<int> &nums, int k) {
+        std::sort(nums.begin(), nums.end(), greater<int>());
+        return nums[k - 1];
+    }
+};
