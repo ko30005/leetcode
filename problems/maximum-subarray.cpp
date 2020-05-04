5e5875154f2f2509e8fe6453e594213507bbf4be
zo0005 <aaaaaright123@gmail.com>
2020-05-04 14:35:38 +0900

    fix Maximum Subarray

--- problems/maximum-subarray.cpp
+++ problems/maximum-subarray.cpp
@@ -4,11 +4,7 @@ using namespace std;
 class Solution {
   public:
     int maxSubArray(vector<int> &nums) {
-        if(nums.size() == 0) {
-            return 0;
-        }
-
-        int maxSum = nums[0], curSum = nums[0];
+        int curSum = nums[0], maxSum = nums[0];
         for(int i = 1; i < nums.size(); i++) {
             curSum = max(nums[i], curSum + nums[i]);
             maxSum = max(maxSum, curSum);
