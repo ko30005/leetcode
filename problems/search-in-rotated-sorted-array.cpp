515bcb42c690781e10c578c39ee09b9113885f4f
zo0005 <aaaaaright123@gmail.com>
2020-05-23 12:10:24 +0900

    fix Search in Rotated Sorted Array

--- problems/search-in-rotated-sorted-array.cpp
+++ problems/search-in-rotated-sorted-array.cpp
@@ -3,44 +3,26 @@ using namespace std;
 
 class Solution {
   public:
-    int searchTemplate(vector<int> &nums, int target, int left, int right) {
+    int search(vector<int> &nums, int target) {
+        int left = 0, right = nums.size() - 1;
         while(left <= right) {
             int mid = left + (right - left) / 2;
             if(nums[mid] == target) {
                 return mid;
-            } else if(nums[mid] < target) {
-                left = mid + 1;
-            } else if(nums[mid] > target) {
-                right = mid - 1;
+            } else if(nums[mid] >= nums[left]) {
+                if(target >= nums[left] && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if(target <= nums[right] && target > nums[mid]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
             }
         }
         return -1;
-    }
-
-    int search(vector<int> &nums, int target) {
-        if(nums.size() == 0) {
-            return -1;
-        } else if(nums.size() == 1) {
-            if(nums[0] == target) {
-                return 0;
-            }
-            return -1;
-        }
-
-        int rotedI = nums.size();
-        for(int i = 1; i < nums.size(); i++) {
-            if(nums[i - 1] >= nums[i]) {
-                rotedI = i;
-            }
-        }
-
-        int ans = searchTemplate(nums, target, 0, rotedI - 1);
-        if(ans != -1) {
-            return ans;
-        } else if(rotedI == nums.size()) {
-            return -1;
-        }
-
-        return searchTemplate(nums, target, rotedI, nums.size() - 1);
     }
 };
