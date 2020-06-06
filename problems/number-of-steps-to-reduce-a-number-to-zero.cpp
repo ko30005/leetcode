0db2a8051a0cf8524fbef98b6041bdf9ed036e54
zo0005 <aaaaaright123@gmail.com>
2020-06-06 23:28:13 +0900

    add Number of Steps to Reduce a Number to Zero

---
+++ problems/number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int numberOfSteps(int num) {
+        int count = 0;
+        while(num > 0) {
+            if(num % 2 == 0) {
+                num = num / 2;
+            } else {
+                num--;
+            }
+            count++;
+        }
+        return;
+    }
+};
